7fde006764cc3a8b405de2dd3ae99623c06bb2bc
bryan <bryan@localhost.localdomain>
2017-05-06 07:35:31 -0500

    finished section 3

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.use(bodyparser.urlencoded({extended: false}));
 app.use(bodyparser.json());
 
 app.use(express.static('public'));
+app.use('/bootstrap',express.static('node_modules/bootstrap/dist'));
+app.use('/jquery',express.static('node_modules/jquery/dist'));
 app.use('/',routes);
 
 app.use(function(error,request,response,next){
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var {db,Place,Hotel,Activity,Restaurant} = require('../db');
+var util = require('../util');
+var Promise = require('bluebird');
 
 router.get('/',function(request,response){
-    response.render('index');
+  var hotels,restaurants,activities;
+
+  const QUERY_HOTEL = {attributes: ['name']};
+  const QUERY_ACTIVITY = {attributes: ['name']};
+  const QUERY_RESTAURANT = {attributes: ['name']};
+
+  Promise.all([     Hotel.findAll(QUERY_HOTEL),
+                 Activity.findAll(QUERY_ACTIVITY),
+               Restaurant.findAll(QUERY_RESTAURANT)])
+  .then(function(arr){
+    function extract(agg,e){
+      agg.push(e.dataValues.name);
+      return agg;
+    }
+
+    hotels = arr[0].reduce(extract,[]);
+    activities = arr[1].reduce(extract,[]);
+    restaurants = arr[2].reduce(extract,[]);
+
+    response.render('index',{hotels,activities,restaurants});
   });
+});
 
 module.exports = router;
